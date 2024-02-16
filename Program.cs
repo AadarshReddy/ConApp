119de356ca9cc36d39b9c3968e071e8fea2744ef
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-16 09:35:42 +0530

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+
+Console.WriteLine("Welcome to Number Sustem");
+Console.WriteLine("Even Number");
+Number.EvenNum();
+Console.WriteLine("Odd Number");
+Number.OddNum();
